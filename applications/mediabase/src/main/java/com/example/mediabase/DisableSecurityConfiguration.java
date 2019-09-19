61cc73ff9bc46bbc83763c233f94390f1953570d
Sayali Khare <sayali.khare@perficient.com>
2019-09-19 14:57:19 -0400

    Added Security

--- applications/mediabase/src/main/java/com/example/mediabase/DisableSecurityConfiguration.java
+++ applications/mediabase/src/main/java/com/example/mediabase/DisableSecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.example.mediabase;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.web.client.RestOperations;
 
 @EnableWebSecurity
 @Configuration
@@ -12,6 +18,12 @@ public class DisableSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestOperations restOperations(OAuth2ProtectedResourceDetails resourceDetails, OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(resourceDetails, oauth2ClientContext);
     }
 
 }
